0b4b200e5f16c1eaeb6288a6fcad32c57c20990c
sidbot2024 <155956430+sidbot2024@users.noreply.github.com>
2024-01-09 19:49:36 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,7 +2,6 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,7 +9,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/notanimechidori'>Bucker 🇳🇬</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {version}</a>\n○ Source Code : <a href='https://t.me/notanimechidori'>Click here</a>\n○ Channel 1🧏 : <a href='https://t.me/AnimeChidori'>Anime Chidori </a>\n○ Channel 2💯 : <a href='https://t.me/Ongoing_Aniime'>Ongoing Anime</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
