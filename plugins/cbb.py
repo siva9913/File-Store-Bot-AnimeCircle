287cba686afad8a927a129350808b9f6a66237fa
sidbot2024 <155956430+sidbot2024@users.noreply.github.com>
2024-01-10 09:45:51 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,7 +2,7 @@
 
 from pyrogram import __version__
 from bot import Bot
-from bot import OWNER_ID
+from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
