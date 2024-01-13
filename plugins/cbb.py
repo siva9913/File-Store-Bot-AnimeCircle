ce427264916f9029bbbc93673a0548f9785c935c
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-13 18:10:10 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -14,8 +14,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close"),
-                        InlineKeyboardButton("⬅️ Back", callback_data = "about")
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
             )
