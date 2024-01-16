43bf03b05594d3f5a865efcc072b1985263ff6a8
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-16 22:51:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -94,9 +94,20 @@ async def start_command(client: Client, message: Message):
         InlineKeyboardButton('⛩️ OUR OTHER CHANNELS ⛩️', url='https://t.me/animemoviesr/3171'),
     ]])
 
-        
-        
-           await message.reply_photo(Config.START_PIC,
+if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, text = START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+            reply_markup = reply_markup,
+            disable_web_page_preview = True,
+            quote = True
+        )       
+  
+        await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
