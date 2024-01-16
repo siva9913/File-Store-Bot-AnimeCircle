8a5f3fbce9ea362a408356aec3fdfabc974226f5
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-16 23:40:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -107,7 +107,8 @@ if Config.START_PIC:
             quote = True
         )       
   
-   else await message.reply_text(
+else:
+    await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
