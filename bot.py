88b1bd5ab919073a71825421cbf9fb4537479016
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-16 16:24:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -54,7 +54,7 @@ class Bot(Client):
                     await self.export_chat_invite_link(FORCE_SUB_CHANNEL1)
                     link = (await self.get_chat(FORCE_SUB_CHANNEL1)).invite_link
     
-                self.invitelink = link
+                self.invitelink2 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
