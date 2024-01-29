1b96ccb3e89313ee7ecfba5f55fe945d0fd8747f
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-29 19:03:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -15,23 +15,10 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('🔒 Close', callback_data = 'close'),
-                        InlineKeyboardButton('⬅️ Back', callback_data = 'back')
-                    ]])
-                  )
-     elif data == "back":
-        await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/shidoteshika1'>The king 🜲</a>\n○ Language : <code>Python3</code>\n○ Library : Pyrogram asyncio {__version__}</a>\n○ Support Group : <a href='https://t.me/chatbox480'>Aɴɪᴍᴇ Cɪʀᴄʟᴇ 🜲</a></b>",
-            disable_web_page_preview = True,
-            reply_markup = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton("⛩️ OUR OTHER CHANNELS ⛩️", url = "https://t.me/animemoviesr/3171")
-                    ],  [
-                        InlineKeyboardButton("🤖 About", callback_data = "about"),
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]])
-              )
+                  )
+
       elif data == "close":
         await query.message.delete()
         try:
