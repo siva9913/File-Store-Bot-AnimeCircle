0430ecd00a7b566c02e0b92b0c80e58b63078523
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-30 16:07:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 
 from bot import Bot
-from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
+from config import ADMINS, FORCE_MSG, Txt, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
@@ -85,28 +85,18 @@ async def start_command(client: Client, message: Message):
                 pass
         return
     else:
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("🤖 Click Here to know about Me", callback_data = "about")
-                    ], [
-        InlineKeyboardButton('⛩️ VISIT MY OWNERS CHANNELS ⛩️', url='https://t.me/animemoviesr/3171'),
-                ], [
-                    InlineKeyboardButton("🔒 Close Messages !", callback_data = "close")
-    ]])  
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
-        return
+        @Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    user = message.from_user
+    await db.add_user(client, message)                
+    button = InlineKeyboardMarkup([[
+        InlineKeyboardButton('🎛 Aʙᴏᴜᴛ', callback_data='about'),
+        InlineKeyboardButton('🛠 Hᴇʟᴩ', callback_data='help')
+    ]])
+    if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
 
 
    
