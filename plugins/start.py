482a3926fc1acc64b34af730eecdfa5afedcaf38
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-30 15:52:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -126,27 +126,16 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(text="Jᴏɪɴ Cʜᴀɴɴᴇʟ", url=client.invitelink),
             InlineKeyboardButton(text="Jᴏɪɴ Gʀᴏᴜᴘ", url=client.invitelink2)    
-        ],
-        [
-            InlineKeyboardButton(
-                text='♻️ Try Again',
-                url=f"https://t.me/{client.username}?start={message.command[1]}"
-            )
         ]
     ]
     try:
-        await message.reply(
-            text=FORCE_MSG.format(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=None if not message.from_user.username else '@' + message.from_user.username,
-                mention=message.from_user.mention,
-                id=message.from_user.id
-            ),
-            reply_markup=InlineKeyboardMarkup(buttons),
-            quote=True,
-            disable_web_page_preview=True,
-            photo="https://telegra.ph/file/1b27c7a2b6a0971bbb561.jpg"
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text = '♻️ Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+            ]
         )
     except IndexError:
         pass
