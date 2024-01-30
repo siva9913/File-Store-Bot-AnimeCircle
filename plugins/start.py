27c2776691d457569b743f6b7f9ed20249381a7a
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-30 18:51:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -140,6 +140,19 @@ async def not_joined(client: Client, message: Message):
     except IndexError:
         pass
 
+          await message.reply(
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
+    )
+
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
