88b10be45aff189d39d43e6c590ce366024a31d4
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-01-30 18:48:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,15 +88,25 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("🤖 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton('⛩️ OUR OTHER CHANNELS ⛩️', url='https://t.me/animemoviesr/3171')
                 ], [
-        InlineKeyboardButton('⛩️ OUR OTHER CHANNELS ⛩️​', url='https://t.me/animemoviesr/3171'),
-    ]])
-    if Config.START_PIC:
-        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button)       
-    else:
-        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True)
+                     InlineKeyboardButton("🤖 About Me", callback_data = "about"),
+                     InlineKeyboardButton("🔒 Close", callback_data = "close")
+         ]])
+    
+        await message.reply_text(
+            text = START_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+            reply_markup = reply_markup,
+            disable_web_page_preview = True,
+            quote = True
+        )
+        return
 
 
    
